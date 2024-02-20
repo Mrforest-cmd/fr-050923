19590a5a86c886934ac24d033231d16ddf857785
Mr.forest <lehan020809@gmail.com>
2024-02-20 18:06:40 +0200

    a

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public static class Algorithms
+    {
+        public static List<object> OpenArr(List<object> arr, int pos = 0, List<object> result = null)
+        {
+            if(result == null)
+            {
+                result = new List<object>();
+            }
+            if (arr[pos] is int) {
+                result.Add(arr[pos++]);
+                return OpenArr(arr, pos, result);
+            }
+            if (arr[pos] is List<object>) {
+                pos = 0;
+                return OpenArr((List<object>)arr[pos], pos, result);
+            }
+            return result;
+        }
+        /*public static List<object> OpenArr(List<object> arr)
+        {
+            List<object> result = new List<object>();
+            foreach (var item in arr)
+            {
+                if (item is List<object>)
+                {
+                    foreach (var variable in OpenArr((List<object>)item)) {
+                        result.Add(variable);
+                    }
+                    //result.AddRange(OpenArr((List<object>)item));
+                }
+                else
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }*/
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                List<object> arr = new List<object> { 1, 2, 3, 4, 5, new List<object>{ 6, 7, 8, 9, 10, new List<object> { 11, 12, 13, 14, 15 } } };
+                List<object> result = Algorithms.OpenArr(arr);
+                foreach(var item in result)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public interface IBook
+    {
+        string Name { get; set; }
+        string Description { get; set; }
+        string Author { get; set; }
+        ushort Year { get; set; }
+    }
+    public class Book : IBook
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public ushort Year { get; set; }
+
+        public Book(string Name, string desc, string auth, ushort issue) {
+            this.Name = Name;
+            this.Description = desc;
+            this.Author = auth;
+            this.Year = issue;
+        }   
+
+    }
+    public interface ILibrary
+    {
+        void AddBook(Book book);
+        void RmBook(string Name);
+        void ShowAllBooks();
+        
+    }
+    class Library : ILibrary
+    {
+        public List<Book> Books = new List<Book>();
+        public Library() {
+            
+        }
+        public void AddBook(Book book)
+        {
+            Books.Add(book);
+        }
+        public void RmBook(string Name)
+        {
+            foreach (Book book in Books) {
+            if(book.Name == Name)
+                {
+                    Books.Remove(book);
+                }
+            }
+        }
+        public void ShowAllBooks()
+        {
+            foreach (Book book in Books) {
+                Console.WriteLine($"Title: {book.Name}" +
+                    $"Author: {book.Author}\n" +
+                    $"Description: {book.Description}\n" +
+                    $"Year of issue: {book.Year}\n");
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library lib = new Library();
+            lib.AddBook(new Book("a","b","c",100));
+            lib.AddBook(new Book("d", "e", "f", 150));
+            lib.ShowAllBooks();
+            lib.RmBook("a");
+            lib.ShowAllBooks();
+        }
+    }
+}
+
+/*
+Створіть інтерфейс IBook, який визначає методи для отримання інформації про книгу (наприклад, назва, автор, рік видання).
+
+Реалізуйте цей інтерфейс у класах, що представляють окремі книги. Кожен клас повинен надати конкретну реалізацію методів.
+
+Створіть інтерфейс ILibrary, який визначає методи для додавання та видалення книги з бібліотеки, а також для отримання списку всіх книг у бібліотеці.
+
+Реалізуйте цей інтерфейс у класі Library, який буде представляти справжню бібліотеку. Клас повинен мати колекцію книг та методи для взаємодії з бібліотекою.
+
+Створіть основний код програми, в якому створюється об'єкт бібліотеки, додаються кілька книг, виводиться інформація про всі книги та видаляється одна з книг.
+ */
