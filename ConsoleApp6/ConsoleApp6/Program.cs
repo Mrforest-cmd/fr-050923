be9c41c922428cf3e0be4f9fffad099ba1c74727
Mr.forest <lehan020809@gmail.com>
2024-02-22 18:05:11 +0200

    l

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using smh;
+
+namespace ConsoleApp6
+{
+    public abstract class Contact
+    {
+        string Name;
+        string PhoneNumber;
+    }
+    class PersonalContact : Contact
+    {
+        PersonalContact(string N, string PN) { ; }
+    }
+    class BuisnessContact : Contact
+    {
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+/*
+Розробіть програму для зоопарку, яка моделює роботу з тваринами. 
+
+Створіть інтерфейс "Тварина" (Animal), який містить методи "Голосити" та "Їсти". 
+Створіть абстрактний клас "Ссавець" (Mammal), який наслідується від інтерфейсу "Тварина" та містить властивість "Тип Харчування". 
+Створіть класи "Собака" (Dog), "Кіт" (Cat) і "Слон" (Elephant), які наслідуються від "Ссавця". 
+
+У кожного з цих класів має бути своя реалізація методів "Голосити" та "Їсти". Додайте можливість зберігати та виводити інформацію про тварин у зоопарку
+. 
+Використайте колекції для зберігання об'єктів тварин. 
+Реалізуйте взаємодію з користувачем, дозволяючи додавати нових тварин, визначати, які тварини знаходяться в зоопарку, та викликати методи "Голосити" та "Їсти" для кожної тварини.
+ */
---
+++ ConsoleApp6/ConsoleApp6/smh.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smh
+{
+    public interface IAnimal
+    {
+
+        void Sound();
+        void Eat();
+    };
+    public abstract class Mammal : IAnimal
+    {
+        public Mammal() { }
+
+        public string type_of_food;
+
+        public string description;
+        public abstract void Eat();
+
+        public abstract void Sound();
+
+    }
+}
